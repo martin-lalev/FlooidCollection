57cacff84d9725cf437d580d5252e31ef32b4063
Your Name <you@example.com>
2019-07-02 10:57:18 +0300

    Initial Commit

---
+++ FlooidCollection/FlooidCollection.h
@@ -0,0 +1,19 @@
+//
+//  FlooidCollection.h
+//  FlooidCollection
+//
+//  Created by Martin Lalev on 2.07.19.
+//  Copyright © 2019 Martin Lalev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlooidCollection.
+FOUNDATION_EXPORT double FlooidCollectionVersionNumber;
+
+//! Project version string for FlooidCollection.
+FOUNDATION_EXPORT const unsigned char FlooidCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlooidCollection/PublicHeader.h>
+
+
